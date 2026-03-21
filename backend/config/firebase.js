c217e10c2df71ec82c9ef93eaec0df9b443ad789
Aia Garcia <aiagarciaa2205@gmail.com>
2026-03-21 20:03:58 +0800

    fix

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
